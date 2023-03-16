19c4f38574fbeb0423b0e1c500745c7c0659c035
Your Name <you@example.com>
2023-03-16 15:27:41 +0500

    I resloved the internal server error

--- pages/api/blogs.ts
+++ pages/api/blogs.ts
@@ -1,33 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import  * as fs  from 'fs';
-
+import path, { resolve } from 'path';
 
 type Data = {
   name: string
 }
+let root = process.cwd()
+let root1 = root.replace('\\','/')
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-// read the file
 const allData : any[] = []
-let files = await fs.promises.readdir('BlogData')
+let files = await fs.promises.readdir(`${root1}/BlogData`)
 let Jsondata
+
 
 
 for (let index = 0; index < files.length; index++) {
   const element = files[index];
-  Jsondata = await fs.promises.readFile(`BlogData/${element}` , 'utf-8');
+  Jsondata = await fs.promises.readFile(`${root1}/BlogData/${element}`, 'utf-8');
   allData.push(JSON.parse(Jsondata))
 
   
 }
 
 
-  res.send(allData)
+ res.send(allData)
+
 
  
-
   
 }
